c2a74d6c27530f4fab6f8df63a567ea4dc40449c
AndrewIcar <icar08@yandex.ru>
2019-02-22 22:43:39 +0300

    Minimum, Maxixmum from Array

---
+++ src/MinMaxArray.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class MinMaxArray {
+
+
+/*
+Минимаксы в массивах
+*/
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int[] numbers = new int[20];
+
+            int maximum;
+            int minimum;
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = Integer.parseInt(reader.readLine());
+            }
+            maximum = numbers[0];
+            minimum = numbers[0];
+
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] < minimum){
+                    minimum = numbers[i];
+                }
+            }
+
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] > maximum){
+                    maximum = numbers[i];
+                }
+            }
+
+            System.out.print(maximum + " " + minimum);
+        }
+}
