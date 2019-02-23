74c74f9a08ec97c056234e6e73e51b35c2741d04
AndrewIcar <icar08@yandex.ru>
2019-02-23 14:45:09 +0300

    Odd or even Day(from the start of the year)

--- src/oddEvenDate.java
+++ src/oddEvenDate.java
@@ -16,23 +16,26 @@ public class oddEvenDate {
         try {
             parsingDate = format.parse(date);
             System.out.println(parsingDate);
+
+            Date yearStartTime = new Date();
+            yearStartTime.setHours(0);
+            yearStartTime.setMinutes(0);
+            yearStartTime.setSeconds(0);
+            yearStartTime.setDate(1);      // первое число
+            yearStartTime.setMonth(0);     // месяц январь, нумерация для месяцев 0-11
+
+            long msTimeDistance = currentTime.getTime() - yearStartTime.getTime();
+            long msDay = 24 * 60 * 60 * 1000;  //сколько миллисекунд в одних сутках
+
+            int dayCount = (int) (msTimeDistance/msDay); //количество целых дней
+
+            return dayCount%2 != 0 ;
         }catch (ParseException e) {
             System.out.println("Нераспаршена с помощью " + format);
+            return false;
         }
 
-        Date yearStartTime = new Date();
-        yearStartTime.setHours(0);
-        yearStartTime.setMinutes(0);
-        yearStartTime.setSeconds(0);
-        yearStartTime.setDate(1);      // первое число
-        yearStartTime.setMonth(0);     // месяц январь, нумерация для месяцев 0-11
 
-        long msTimeDistance = currentTime.getTime() - yearStartTime.getTime();
-        long msDay = 24 * 60 * 60 * 1000;  //сколько миллисекунд в одних сутках
-
-        int dayCount = (int) (msTimeDistance/msDay); //количество целых дней
-
-        return dayCount%2 != 0 ;
 
     }
 }
