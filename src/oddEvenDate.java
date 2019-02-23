a76f4c9b068f040c518803d610ace5d5618e3068
AndrewIcar <icar08@yandex.ru>
2019-02-23 14:02:47 +0300

    Odd or even Day(from the start of the year)

---
+++ src/oddEvenDate.java
@@ -0,0 +1,38 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class oddEvenDate {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("FEBRUARY 2 1980"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        Date currentTime = new Date();
+        SimpleDateFormat format = new SimpleDateFormat ("MM dd yyyy");
+
+        System.out.print("Строка " + date + " распаршена как ");
+        Date parsingDate;
+        try {
+            parsingDate = format.parse(date);
+            System.out.println(parsingDate);
+        }catch (ParseException e) {
+            System.out.println("Нераспаршена с помощью " + format);
+        }
+
+        Date yearStartTime = new Date();
+        yearStartTime.setHours(0);
+        yearStartTime.setMinutes(0);
+        yearStartTime.setSeconds(0);
+        yearStartTime.setDate(1);      // первое число
+        yearStartTime.setMonth(0);     // месяц январь, нумерация для месяцев 0-11
+
+        long msTimeDistance = currentTime.getTime() - yearStartTime.getTime();
+        long msDay = 24 * 60 * 60 * 1000;  //сколько миллисекунд в одних сутках
+
+        int dayCount = (int) (msTimeDistance/msDay); //количество целых дней
+
+        return dayCount%2 != 0 ;
+
+    }
+}
