d60443739f3046a036cf0d9927cead80075250e0
AndrewIcar <icar08@yandex.ru>
2019-02-25 22:47:26 +0300

    >_<

---
+++ src/StackTrace.java
@@ -0,0 +1,38 @@
+public class StackTrace {
+
+        public static void main(String[] args) throws Exception {
+            method1();
+        }
+        public static String method1() {
+            method2();
+            StackTraceElement[] StackTrace = Thread.currentThread().getStackTrace();
+            System.out.println(StackTrace[2].getMethodName());
+            return StackTrace[2].getMethodName();
+        }
+        public static String method2() {
+            method3();
+            StackTraceElement[] StackTrace = Thread.currentThread().getStackTrace();
+            System.out.println(StackTrace[2].getMethodName());
+            return StackTrace[2].getMethodName();
+        }
+        public static String method3() {
+            method4();
+            StackTraceElement[] StackTrace = Thread.currentThread().getStackTrace();
+            System.out.println(StackTrace[2].getMethodName());
+            return StackTrace[2].getMethodName();
+        }
+        public static String method4() {
+            method5();
+            StackTraceElement[] StackTrace = Thread.currentThread().getStackTrace();
+            System.out.println(StackTrace[2].getMethodName());
+            return StackTrace[2].getMethodName();
+        }
+        public static String method5() {
+            {
+                StackTraceElement[] StackTrace = Thread.currentThread().getStackTrace();
+                System.out.println(StackTrace[2].getMethodName());
+                return StackTrace[2].getMethodName();
+            }
+        }
+    }
+
---
+++ src/vowelСonsonants.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.*;
+
+
+public class vowelСonsonants {
+
+/*
+Гласные и согласные
+*/
+        public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+        public static void main(String[] args) throws Exception {
+            Scanner keyboard = new Scanner(System.in);
+            //ArrayList<String> list = new ArrayList<String>();
+            String list = keyboard.nextLine();
+            char[] charList = list.toCharArray();
+
+            for(int i = 0; i<charList.length; i++){
+                if(isVowel(charList[i])){
+                    System.out.print(charList[i]+" ");
+                }
+
+            }
+            System.out.println();
+            for(int i = 0; i<charList.length; i++){
+                if(!isVowel(charList[i])){
+                    System.out.print(charList[i]+" ");
+                }
+
+            }
+        }
+
+        // метод проверяет, гласная ли буква
+        public static boolean isVowel(char c) {
+            c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+            for (char d : vowels)   // ищем среди массива гласных
+            {
+                if (c == d)
+                    return true;
+            }
+            return false;
+        }
+    }
+
